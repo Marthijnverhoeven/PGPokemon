6eb2524c20aa2c4abf42a84927a4919c640267f8
42IN04SOf <fahiemkarso@gmail.com>
2016-04-04 15:40:32 +0200

    geolocation pinpointer

--- www/js/view/NearbyPokemonView.js
+++ www/js/view/NearbyPokemonView.js
@@ -6,7 +6,7 @@ var NearbyPokemonView = function(nearbyPokemonCS) {
 				`<h1>${pokemon.name}</h1>` +
 			`</div>` +
 			`<div class="ui-grid-a">` +
-				`<a href="geo:${cache.coords.lat},${cache.coords.long}" class="ui-block-a ui-btn ui-corner-all">Maps</a>` +
+				`<a href="geo:${cache.coords.lat},${cache.coords.long}?q=${cache.coords.lat},${cache.coords.long}" class="ui-block-a ui-btn ui-corner-all">Maps</a>` +
 				`<button type="button" class="ui-block-b ui-btn ui-corner-all">Vangen</button>` +
 			`</div>` +
 		`</li>`;
@@ -21,7 +21,7 @@ var NearbyPokemonView = function(nearbyPokemonCS) {
 		return jQueryNode;
 	}
 	var createInitialNode = function(pos) {
-		return $(`<a href="geo:${pos.coords.latitude},${pos.coords.longitude}" class="ui-btn ui-corner-all">Mijn locatie</a>`);
+		return $(`<a href="geo:${pos.coords.latitude},${pos.coords.longitude}?q=${pos.coords.latitude},${pos.coords.longitude}" class="ui-btn ui-corner-all">Mijn locatie</a>`);
 	}
 	var appendCaches = function(container, pokemons, caches, callback) {
 		for(var i =0; i < caches.length; i++) {
@@ -37,7 +37,7 @@ var NearbyPokemonView = function(nearbyPokemonCS) {
 	var calcCacheDistance = function(pos, cache) {
 		console.log(arguments);
 		cache.catchable = true;
-		var distance = 100;
+		var distance = 0.01;
 		var sortedLong = getSorted(pos.coords.longitude, cache.coords.long);
 		var sortedLat = getSorted(pos.coords.latitude, cache.coords.lat); 
 		if(sortedLat.max - sortedLat.min > distance || sortedLong.max - sortedLong.min > distance) {
