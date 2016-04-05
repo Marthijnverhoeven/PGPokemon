95ed660221677d41375906205117b955d0f6f2af
42IN04SOf <fahiemkarso@gmail.com>
2016-04-05 12:17:41 +0200

    pokedetail fix, sprites added, link to external

--- www/js/view/PokeDetailView.js
+++ www/js/view/PokeDetailView.js
@@ -1,13 +1,36 @@
 var PokeDetailView = function(containerSelector) {
 	var self = this;
 	var helper = {
+		alpha: ["a", "b"],
+		createLinkHTML: function(id) {
+			return `<div class="ui-body ui-body-a ui-corner-all">` +
+					`<a href="http://www.pokemon.com/us/pokedex/${id}" class="ui-btn ui-corner-all">Pokedex entry</a>` +
+				`</div>`;
+		},
+		createTypeHTML: function(types) {
+			var self = this;
+			var typeHTML = '';
+			for(var i = 0; i < types.length; i++) {
+				typeHTML += `<div class="ui-block-${self.alpha[i]}"><div class="poke-type ${types[i].type}"></div></div>`;
+			}
+			return `<div class="ui-body ui-body-a ui-grid-a ui-corner-all">` + 
+					typeHTML + 
+				`</div>`; 
+		},
 		createPokemonNode: function(pokemon) {
-			return $('<div>', { class: "ui-content ui-content-a"})
-				.append($('<h2>').html(pokemon.name))
-				.append($('<img>', { 
-					class: "poke-image",
-					src: pokemon.sprites.front_default 
-				}));
+			var self = this;
+			var typeAppender = self.createTypeHTML(pokemon.types);
+			var linkAppender = self.createLinkHTML(pokemon.id);
+			console.log(typeAppender);
+			var html = `<div class="ui-body ui-body-a">` +
+					`<h2>${pokemon.name}</h2>` +
+					`<div class="ui-body ui-body-a ui-corner-all">` +
+						`<img class="poke-image" src="${pokemon.sprites.front_default}" />` +
+					`</div>` +
+					typeAppender +
+					linkAppender +
+				`</div>`;
+			return $(html);
 		},
 		createErrorNode: function() {
 			
